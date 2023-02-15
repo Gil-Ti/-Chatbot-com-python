43c57e09a7e684e8eff2059a93afb4d27dc31214
Gileard T Santos <111284002+Gil-Ti@users.noreply.github.com>
2023-02-14 21:27:39 -0300

    Importando bibliotecas

---
+++ Chatbot .py
@@ -0,0 +1,10 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+# isso aqui só precisa para corrigir o bug
+from spacy.cli import download
+
+download("en_core_web_sm")
+
+class ENGSM:
+    ISO_639_1 = 'en_core_web_sm'
