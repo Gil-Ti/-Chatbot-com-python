db40f2745d9b12630be5f3e98d2cc02c65e81613
Gileard T Santos <111284002+Gil-Ti@users.noreply.github.com>
2023-02-15 21:21:56 -0300

    Iniciando o código

--- Chatbot .py
+++ Chatbot .py
@@ -8,3 +8,25 @@ download("en_core_web_sm")
 
 class ENGSM:
     ISO_639_1 = 'en_core_web_sm'
+
+
+chatbot = ChatBot("BotLira", tagger_language=ENGSM)
+
+conversa = [
+    "oi",
+
+]
+
+trainer = ListTrainer(chatbot)
+trainer.train(conversa)   
+
+
+while True:
+    mensagem = input("Mande uma mensagem para o chatbot:")
+    if mensagem == "parar":
+        break
+    resposta = chatbot.get_response(mensagem)
+    print(resposta)
+
+
+chatbot.storage.drop()
