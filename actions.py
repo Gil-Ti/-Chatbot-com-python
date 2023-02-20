27a076736a551bf9fffb56158c12a96bdaf64401
Gileard Teixeira <111284002+Gil-Ti@users.noreply.github.com>
2023-02-20 19:42:55 -0300

    Iniciando processamento de linguagem natural (NLP)
    Usar técnicas de processamento de linguagem natural (NLP) pode ajudar a melhorar a precisão e relevância das respostas geradas pelo chatbot. Existem diversas bibliotecas de NLP em Python que podem ser usadas para implementar essas técnicas. Algumas das bibliotecas mais populares incluem:
    
    spaCy: uma biblioteca de NLP para Python que oferece recursos avançados, como análise sintática, reconhecimento de entidades nomeadas e vetores de palavras pré-treinados.
    NLTK: a biblioteca de processamento de linguagem natural mais conhecida em Python, com recursos para tokenização, análise de sintaxe, classificação de texto e muito mais.
    TextBlob: uma biblioteca de NLP fácil de usar que oferece recursos para processamento de texto, análise de sentimento, classificação de texto e muito mais.
    gensim: uma biblioteca de NLP para modelagem de tópicos e análise de semântica latente.
    Para usar essas bibliotecas com o seu chatbot, você precisará treinar o modelo com dados de treinamento relevantes para o tipo de perguntas e respostas que você deseja que o chatbot entenda e responda. Você também pode usar modelos pré-treinados disponíveis na web para alguns casos de uso comuns, como reconhecimento de entidades nomeadas e análise de sentimento.

--- actions.py
+++ actions.py
@@ -1,12 +1,39 @@
-import random
+import speech_recognition as sr
+import pyttsx3
 
-def chatbot():
-    user_input = input("Olá, como posso ajudá-lo hoje? ")
+# Define a linguagem do reconhecimento de voz
+r = sr.Recognizer()
 
-    responses = ["Entendo", "Por favor, continue", "Isso é interessante", "Poderia me contar mais?"]
+# Configura a síntese de voz
+engine = pyttsx3.init()
 
-    while user_input != "sair":
-        print(random.choice(responses))
-        user_input = input()
+def chatbot(text):
+    if "olá" in text.lower():
+        return "Olá, como posso ajudar?"
+    elif "como você está" in text.lower():
+        return "Estou bem, obrigado por perguntar. E você?"
+    elif "qual é a sua idade" in text.lower():
+        return "Eu sou um programa de computador, então não tenho idade."
+    elif "você pode me contar uma piada" in text.lower():
+        return "Por que o computador foi ao médico? Porque estava com vírus!"
+    elif "Quem é o responsável pela retenção tributária?" in text.lower():
+        return "O responsável pela retenção tributária é aquele que efetua o pagamento ao contribuinte. Por exemplo, a empresa contratante é responsável por reter e recolher os tributos quando realiza o pagamento a um prestador de serviço."
+    elif "Como é feito o cálculo da retenção tributária?" in text.lower():
+        return "O cálculo da retenção tributária é feito sobre o valor total da operação ou serviço contratado, utilizando-se a alíquota específica de cada tributo. Essa alíquota varia de acordo com a natureza da operação e a legislação tributária."
+    elif "O que acontece se a retenção tributária não for feita corretamente?" in text.lower():
+        return "Se a retenção tributária não for feita corretamente, o responsável pelo pagamento poderá ser autuado e ter que arcar com multas e juros de mora. Além disso, o contribuinte que não teve o tributo retido na fonte poderá ser cobrado pelo valor devido, acrescido de juros e correção monetária."
+    else:
+        return "Desculpe, não entendi o que você falou"
 
-chatbot()
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)
+    print("Fale algo: ")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio, language='pt-BR')
+        response = chatbot(text)
+        print(response)
+        engine.say(response)
+        engine.runAndWait()
+    except:
+        print("Desculpe, não entendi o que você falou")
